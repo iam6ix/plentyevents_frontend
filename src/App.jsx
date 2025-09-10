d30ffd972b07138de91ba8bb1e1c7638b56a6e13
iam6ix <osondukaleb@gmail.com>
2025-09-10 20:28:02 +0100

    Vendors update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import Footer from "./components/Footer";
 import VendorGrid from "./components/VendorGrid";
